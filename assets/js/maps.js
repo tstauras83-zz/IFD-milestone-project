a382e4271bca3873ec1608c2397eaeaaed310fb9
Tauras Semaska <tstauras100@gmail.com>
2018-11-29 12:39:59 +0000

    added map, and narbar logo image.

---
+++ assets/js/maps.js
@@ -0,0 +1,9 @@
+function initMap() {
+    var map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 6,
+        center: {
+            lat: 54.8985,
+            lng: 23.9036
+        }
+    });
+}
