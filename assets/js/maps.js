3323e07d5dc6da72d8af9594e48d75c6e3ca6b18
Tauras Semaska <tstauras100@gmail.com>
2018-12-12 13:55:06 +0000

    searchBar working with auto marker on search locations

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -2,60 +2,80 @@ var map;
 var markers = [];
 
 
+
+
 function initMap() {
     var mapCenter = new google.maps.LatLng(54.8985, 23.9036);
 
     map = new google.maps.Map(document.getElementById('map'), {
         center: mapCenter,
-        zoom: 6
+        zoom: 6,
+        streetViewControl: false,
+        disableDefaultUI: true,
     });
 
     map.addListener('click', function(event) {
         addMarker(event.latLng);
     });
-    
-/*/ ///////////////////////////////////searchBox///////////////////////////////////////*/
-
     addMarker(mapCenter);
+
+
+
+
+
+    /*////////////////////////////////////searchBox///////////////////////////////////////*/
+
+
 
     var card = document.getElementById('address-searchBar');
     var input = document.getElementById('searchBoxx');
-
-
-    map.controls[google.maps.ControlPosition.TOP_RIGHT].push(card);
-
-    var autocomplete = new google.maps.places.Autocomplete(input);
-
-    // Bind the map's bounds (viewport) property to the autocomplete object,
-    // so that the autocomplete requests use the current map bounds for the
-    // bounds option in the request.
-    autocomplete.bindTo('bounds', map);
-
-    // Set the data fields to return when the user selects a place.
-    autocomplete.setFields(
-        ['address_components', 'geometry', 'icon', 'name']);
+    var searchBox = new google.maps.places.SearchBox(input);
 
 
 
-    autocomplete.addListener('place_changed', function() {
-        var place = autocomplete.getPlace();
-        if (!place.geometry) {
-            // User entered the name of a Place that was not suggested and
-            // pressed the Enter key, or the Place Details request failed.
-            window.alert("No details available for input: '" + place.name + "'");
+
+    map.addListener('bounds_changed', function() {
+        searchBox.setBounds(map.getBounds());
+    });
+
+    var markerz = [];
+    searchBox.addListener('places_changed', function() {
+        var places = searchBox.getPlaces();
+
+        if (places.length == 0) {
             return;
         }
 
-        // If the place has a geometry, then present it on a map.
-        if (place.geometry.viewport) {
-            map.fitBounds(place.geometry.viewport);
-        }
-        else {
-            map.setCenter(place.geometry.location);
-            map.setZoom(17); // Why 17? Because it looks good.
-        }
+        markerz.forEach(function(marker) {
+            marker.setMap(null);
+        });
+        markerz = [];
+
+        var bounds = new google.maps.LatLngBounds();
+        places.forEach(function(place) {
+            if (!place.geometry) {
+                console.log("Returned place contains no geometry");
+                return;
+            }
+
+            markerz.push(new google.maps.Marker({
+                map: map,
+                draggable:true,
+                title: place.name,
+                position: place.geometry.location
+            }));
+
+            if (place.geometry.viewport) {
+                bounds.union(place.geometry.viewport);
+            }
+            else {
+                bounds.extend(place.geometry.location);
+            }
+        });
+        map.fitBounds(bounds);
     });
 }
+
 
 
 /*////////////////////////////////////map markers///////////////////////////////////////*/
@@ -75,7 +95,9 @@ function callback(results, status) {
 function addMarker(location) {
     var marker = new google.maps.Marker({
         position: location,
-        map: map
+        map: map,
+        title: "undefined location" + location,
+        draggable: true,
     });
     markers.push(marker);
 }
